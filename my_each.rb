361c36edb83c125766f287230419103bb854f154
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-16 00:37:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
 def my_each(array)
   if block_given?
-  i = 0
-  while i < array.length
-  yield(array[1])
-  i += 1
+    i = 0
+    while i < array.length
+    yield(array[1])
+    i += 1
+    end
   end
-else
-  puts "Yessir"
 end
  
